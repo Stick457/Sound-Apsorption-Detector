4f1aa6afc7800da0e01322a98f613c266a042d7a
Stick457 <76454538+Stick457@users.noreply.github.com>
2022-01-12 07:22:35 -0600

    Started on Main Program
    Imported LCD settings and added basic variables

--- SoundApsoptionDetector/SoundApsoptionDetector.ino
+++ SoundApsoptionDetector/SoundApsoptionDetector.ino
@@ -1,11 +1,16 @@
 //1.9.2022
+float frequencies[] = {100, 200, 500, 1500};//The frequencies of incident sound
+float TranSound;//The transmitted sound
+float soundAbsCoe; // The sound absorbed
+
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 void setup() {
-  // put your setup code here, to run once:
-
+  lcd.begin(16, 2);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  lcd.print("Hello, World!");
 }
