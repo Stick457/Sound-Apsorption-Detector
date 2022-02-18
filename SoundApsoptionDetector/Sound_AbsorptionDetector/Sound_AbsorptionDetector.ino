171f24500586622096930c2ecbf592f39c18456e
Stick457 <76454538+Stick457@users.noreply.github.com>
2022-02-17 21:54:40 -0600

    Finished Project
    Finished the Sound Absorption Detector.
    It will play a sound and then record the sound absorbed.

--- SoundApsoptionDetector/Sound_AbsorptionDetector/Sound_AbsorptionDetector.ino
+++ SoundApsoptionDetector/Sound_AbsorptionDetector/Sound_AbsorptionDetector.ino
@@ -1,7 +1,13 @@
 //2.13.2022
+
 float SoundAbs;
 float frequencies[] = {100, 200, 500, 1500};
+
 int  sensorDigitalPin = 3;
+int  sensorAnalogPin = A0;
+float  analogValue;
+int  digitalValue; 
+
 
 void setup() {
   Serial.begin(9600);
@@ -9,9 +15,14 @@ void setup() {
 }
 
 void loop() {
+  analogValue = analogRead(sensorAnalogPin);
+  digitalValue=digitalRead(sensorDigitalPin);
+  
   for(int i = 0; i< 4; i++){
     tone(8, frequencies[i], 500);
+    
+    Serial.println(1 - analogValue/frequencies[i]);// Print out the sound absorped. a = Ea  / Ei  =  1 - Et  / Ei  where “a” is the sound absorption coefficient
 
-    delay(2000);
+    delay(3000);
   }
 }
